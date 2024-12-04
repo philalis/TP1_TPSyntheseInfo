47507332ae09b6c5f7005b90b247310258015306
philalis <153091250+philalis@users.noreply.github.com>
2024-12-04 17:37:28 +0100

    Q4 + debut Q6

--- Main.c
+++ Main.c
@@ -12,11 +12,14 @@ void main(){
 	const static char*  promptMessage = 		"enseash %\n";
 	#define maxCommandLength 1024
 	static char commandbuff[maxCommandLength];
+	char * command=malloc(maxCommandLength);
+	char ** argumentTable= malloc(maxCommandLength);
 
 	write(STDOUT_FILENO,welcomeMessage,strlen(welcomeMessage));
 	while(1){
 		write(STDOUT_FILENO,promptMessage,strlen(promptMessage));
 		 int commandBytes =  read(STDIN_FILENO,commandbuff,maxCommandLength);
+		  command = SetCommandTable(commandbuff,argumentTable);
 		 char test[4];
 		strcpy(test,commandbuff);
 		if(strcmp(test,"exit\n")==0 || strcmp(test,"^D")){
@@ -27,19 +30,55 @@ void main(){
 		int status;
 		if (commandBytes>0){
 			int pid = fork();
-		if(pid ==0){				
-			execlp(commandbuff,commandbuff,(char*)NULL);
-			exit(EXIT_FAILURE);
-		}
+			if(pid2 == 0 ){
+				//execlp(commandbuff,commandbuff,(char*)NULL);
+				execvp(argumentTable[0],argumentTable);
+				exit(EXIT_FAILURE);
+			}
+			else {
+				wait(&status);
+				char * ExitText;
+				if(WIFEXITED(status)){
+					sprintf(ExitText,"%d", WEXITSTATUS(status));
+				}
+				if(WIFSIGNALED(status)){
+					sprintf(ExitText,"%d", WTERMSIG(status));
+				}
+				write(STDOUT_FILENO,ExitText,8);
+				exit(EXIT_SUCCESS);
+			}
 		else {
 			wait(&status);
 		}
 
 		}
 	
-	}
-
-
-
-	
+	}	
 }
+
+char * SetCommandTable(char * commandbuff,char ** argumentTable){
+	char * currentcommand=malloc(maxCommandLength);
+	int nthCommand = 0;
+	int current=0;
+	char * command;
+	for (int i=0;i<strlen(commandbuff);i++){
+		if (commandbuff[i]== ' '|| commandbuff[i]=='\n'){
+			if (nthCommand ==0){
+				command=currentcommand;
+			}
+			argumentTable[nthCommand]=currentcommand;
+			nthCommand+=1;
+			current=0;
+			currentcommand= "";
+		}
+		else{
+			currentcommand[current]=commandbuff[i];
+			current+=1;
+		}
+	}
+	//free(currentcommand);
+	return command;
+}
+
+
+
